f5c235a884d1bf4fd50115554455d5d2fff79b40
Vanderley Junior <vanderley_1998@hotmail.com.br>
2022-11-29 00:53:24 -0300

    feat: adds animation on hint and error-hint

--- src/app/shared/components/vj-input/directives/vj-input.directive.ts
+++ src/app/shared/components/vj-input/directives/vj-input.directive.ts
@@ -1,12 +1,15 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[vjInput]'
 })
 export class VjInputDirective {
 
+  @Input() type: 'text' = 'text';
+
   constructor(public element: ElementRef<HTMLInputElement>) {
     element.nativeElement.id = `vj-input-${new Date().getTime()}`
+    element.nativeElement.type = this.type;
   }
 
 }
