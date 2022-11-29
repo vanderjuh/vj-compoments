8f36e3b4c75ea2d7fb8867c6947b632d5af93e67
Vanderley Junior <vanderley_1998@hotmail.com.br>
2022-11-28 22:24:30 -0300

    feat(Input): adds the autocomplete listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { VjInputData } from './shared/components/vj-input/classes/vj-input-data.class';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vj-compoments';
+
+  list: Array<VjInputData> = [
+    { value: 1, label: 'Test 1' },
+    { value: 2, label: 'Test 2' },
+    { value: 3, label: 'Test 3', disabled: true },
+  ];
 }
--- src/app/shared/components/vj-input/vj-input.component.ts
+++ src/app/shared/components/vj-input/vj-input.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { VjInputData } from './classes/vj-input-data.class';
 
 @Component({
   selector: 'vj-input',
   templateUrl: './vj-input.component.html',
-  styleUrls: ['./vj-input.component.scss']
+  styleUrls: ['./vj-input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VjInputComponent {
   @Input() label?: string;
   @Input() hint?: string;
   @Input() invalid?: boolean;
+  @Input() list?: VjInputData[];
+
+  isLoading!: boolean;
 
   onLabel(): void {
     console.log('fô');
+  }
+
+  onClickAutocompleteItem(event: VjInputData): void {
+    console.log(event)
   }
 }
